1b40d6551871efd731809bcddf79e87a5319c78a
oberkunskyi <oleksii.berkunskyi@ocado.com>
2023-01-20 14:59:18 +0000

    Initial commit

---
+++ src/main/java/com/mamoru/banderabot/BanderaBotApplication.java
@@ -0,0 +1,13 @@
+package com.mamoru.banderabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanderaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanderaBotApplication.class, args);
+	}
+
+}
